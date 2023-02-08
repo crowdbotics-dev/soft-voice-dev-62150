8ea44d57552e6ccc5299139db3cf4a7c8e44847a
Crowdbotics <engineering@crowdbotics.com>
2023-02-08 09:14:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-voice-dev-62150",
+  url: "https://soft-voice-dev-62150.botics.co",
+  api: "https://soft-voice-dev-62150.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
